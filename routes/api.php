7c2d62e10c956aae4cb470f22a0573b285b065cf
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-08-29 14:02:22 -0400

    feat: :sparkles: add user resource

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\{ProductController, CategoryController};
+use App\Http\Controllers\{ProductController, CategoryController, UserController};
 
 Route::post('login', LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('products', ProductController::class);
     Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('users', UserController::class);
 });
 
 Route::get('/user', function (Request $request) {
