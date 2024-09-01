79629382eaf2ae0bb1b7252e04f16a42013afe24
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-01 13:40:40 -0400

    fix: :bug: enable accesses in UserController by laravel api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        Gate::authorize('list_users', User::class);
+        // Gate::authorize('list_users', User::class);
         $users = User::paginate();
         return UserResource::collection($users);
     }
@@ -20,14 +20,14 @@ class UserController extends Controller
     public function show(int $userId)
     {
         $user = $this->user($userId);
-        Gate::authorize('show_user', $user);
+        // Gate::authorize('show_user', $user);
         return UserResource::make($user);
     }
     
     public function update(int $userId, UpdateUserRequest $request)
     {
         $user = $this->user($userId);
-        Gate::authorize('update_user', $user);
+        // Gate::authorize('update_user', $user);
         $user->update($request->validated());
         return UserResource::make($user);   
     }
@@ -35,7 +35,7 @@ class UserController extends Controller
     public function destroy(int $userId)
     {
         $user = $this->user($userId);
-        Gate::authorize('destroy_user', $user);
+        // Gate::authorize('destroy_user', $user);
         $user->delete();
         return response()->noContent(); 
     }
