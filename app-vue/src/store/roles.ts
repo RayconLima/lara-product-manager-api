6125ebf7f39960b9d7417e24acfaa006943a77b0
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-01 11:16:01 -0400

    feat: :sparkles: add roles resource screen

---
+++ app-vue/src/store/roles.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import RolesService from "../infra/services/acl/roles.service"
+
+export const useRoleStore = defineStore('role', {
+    state: () => ({
+        roles: [],
+    }),
+    actions: {
+        async getRoles() {
+            return await RolesService.roles().then((response: any) => {
+                this.roles = response.data.data
+            })
+        },
+    }
+});
---
+++ app-vue/src/ui/pages/ACL/Roles/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <Breadcrumb title="Roles"></Breadcrumb>
+    </div>
+</template>
+<script>
+import Breadcrumb from '../../../components/Breadcrumb.vue';
+export default {
+    name: 'Roles',
+    components: {
+        Breadcrumb
+    }
+}
+</script>
