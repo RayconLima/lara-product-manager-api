f5a6940b2732c574ddd9f749cedb38283f8ec398
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-01 13:35:44 -0400

    fix: :art: correct routes in laravel api

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\MeController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\{ProductController, CategoryController, PermissionController, RoleController, UserController};
 
 Route::post('login', LoginController::class)->name('auth.login');
 Route::post('register', RegisterController::class)->name('auth.register');
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', MeController::class);
+    Route::post('logout', LogoutController::class);
+
     Route::apiResource('products', ProductController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('users', UserController::class);
