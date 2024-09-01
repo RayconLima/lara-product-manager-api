80a028c2977c059a5338b9a08eb82871743409ad
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-01 11:12:49 -0400

    feat: :sparkles: add layout default

--- app-vue/src/ui/layouts/Default.vue
+++ app-vue/src/ui/layouts/Default.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <!-- <Navbar /> -->
+        <Navbar />
 
         <aside id="logo-sidebar"
             class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
             aria-label="Sidebar">
             <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-                <!-- <Sidebar :user="user ?? null" /> -->
+                <Sidebar :user="user ?? null" />
             </div>
         </aside>
 
@@ -19,13 +19,26 @@
 </template>
 <script>
 import { computed, onMounted } from 'vue';
-import { useStore } from 'vuex';
-import { useMeStore } from '@/store/me';
+import { useMeStore } from '../../store/me';
+import Navbar from '../components/Navbar.vue';
+import Sidebar from '../components/Sidebar.vue';
 export default {
     name: 'Default',
     components: {
         Navbar,
         Sidebar
     },
+    setup() {
+        const meStore   = useMeStore();
+        const user      = computed(() => meStore?.user);
+
+        onMounted(async () => {
+            await meStore.getMe();
+        });
+        
+        return {
+            user: user.value
+        }
+    }
 };
 </script>
