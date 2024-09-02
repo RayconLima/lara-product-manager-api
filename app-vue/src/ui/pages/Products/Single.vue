5a3b46039fb4f3700e68b0bc47615f169f749f83
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-02 17:05:06 -0400

    fix: :bug: use helpers in Single Product page in app-vue application

--- app-vue/src/ui/pages/Products/Single.vue
+++ app-vue/src/ui/pages/Products/Single.vue
@@ -7,7 +7,7 @@
                     <ul class="container p-4">
                         <h2 class="px-2 py-2"><span class="font-bold">Produto:</span> {{ product?.name }}</h2>
                         <li><span class="px-2 py-2 text-gray-500 font-semibold">Categoria:</span> {{ product?.category?.name }}</li>
-                        <li><span class="px-2 py-2 text-gray-500 font-semibold">Preço:</span> {{ product?.price }}</li>
+                        <li><span class="px-2 py-2 text-gray-500 font-semibold">Preço:</span> {{ formatMoney(product?.price) }}</li>
                         <li><span class="px-2 py-2 text-gray-500 font-semibold">Descrição:</span> {{ product?.description }}</li>
                         <li><span class="px-2 py-2 text-gray-500 font-semibold">Data de validade:</span> {{ product?.expiration_date }}</li>
                     </ul>
@@ -22,7 +22,7 @@ import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import Breadcrumb from '../../components/Breadcrumb.vue';
 import { useProductStore } from "../../../store/products";
-
+import { formatMoney } from '../../../config/utils/helpers';
 export default {
     name: 'Single',
     components: {
@@ -44,6 +44,7 @@ export default {
 
         return {
             product,
+            formatMoney,
             destroyProduct
         }
     }
