e876ad05eb383ac2e6619de3a4e8230f1d6670d9
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-02 16:02:06 -0400

    feat:

--- app-vue/src/ui/components/Modal.vue
+++ app-vue/src/ui/components/Modal.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-      <button @click="toggleModal" class="px-3 py-1 font-bold dark:text-white bg-blue-500 rounded hover:bg-blue-700">
+      <DynamicButton @click="toggleModal" :color="buttonColor">
         {{ title }}
-      </button>
+      </DynamicButton>
   
       <transition name="modal" v-if="isModalVisible">
         <div
@@ -30,7 +30,7 @@
                   Salvar
                 </button>
                 <button @click="hideModal" type="button"
-                  class="text-gray-500 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-500 dark:bg-red-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
+                  class="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Fechar
                 </button>
               </div>
@@ -43,11 +43,19 @@
   
   <script>
   import { ref } from 'vue';
-  
+  import DynamicButton from './DynamicButton.vue'
+import { string } from 'yup';
   export default {
     name: "Modal",
+    components: {
+      DynamicButton
+    },
     props: {
       title: String,
+      buttonColor: {
+        type: string,
+        default: "bg-blue-500"
+      },
       acceptFunction: Function,
     },
     emits: ['close-modal'],
@@ -80,4 +88,4 @@
   .modal-enter-from,
   .modal-leave-to {
     opacity: 0;
-  }</style>
+  }</style>./DynamicButton.vue
