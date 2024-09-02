41be8ed2bb0369e453b37f784819a09e86ac95b4
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-02 08:14:34 -0400

    refactor: :sparkles: remove logout item by sidebar menu in app-vue application

--- app-vue/src/ui/components/Sidebar.vue
+++ app-vue/src/ui/components/Sidebar.vue
@@ -1,74 +1,74 @@
 <template>
-    <div>
-      <ul class="space-y-2 font-medium">
-        <template v-for="(items, groupName) in groupedSidebar" :key="groupName">
-          <template v-if="groupName !== 'geral' && shouldRenderGroup(items)">
-            <li>
-              <button type="button" v-if="items.title"
-                class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                @click="toggleDropdown(groupName)">
-                <i :class="items.icon"></i>
-                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ items.title }}</span>
-                <svg :class="{ 'transform rotate-180': isDropdownOpen(groupName) }" class="w-3 h-3" aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 4 4 4-4" />
-                </svg>
-              </button>
-              <ul v-show="isDropdownOpen(groupName)" class="py-2 space-y-2">
-                <template v-for="(item, index) in items?.items" :key="index">
-                  <li v-if="hasPermission(item)">
-                    <router-link :to="{ name: item.to }"
-                      class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                      :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
-                      <span class="ml-3">{{ item.title }}</span>
-                    </router-link>
-                  </li>
-                </template>
-              </ul>
+  <div>
+    <ul class="space-y-2 font-medium">
+      <template v-for="(items, groupName) in groupedSidebar" :key="groupName">
+        <template v-if="groupName !== 'geral' && shouldRenderGroup(items)">
+          <li>
+            <button type="button" v-if="items.title"
+              class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              @click="toggleDropdown(groupName)">
+              <i :class="items.icon"></i>
+              <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ items.title }}</span>
+              <svg :class="{ 'transform rotate-180': isDropdownOpen(groupName) }" class="w-3 h-3" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+            <ul v-show="isDropdownOpen(groupName)" class="py-2 space-y-2">
+              <template v-for="(item, index) in items?.items" :key="index">
+                <li v-if="hasPermission(item)">
+                  <router-link :to="{ name: item.to }"
+                    class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
+                    <span class="ml-3">{{ item.title }}</span>
+                  </router-link>
+                </li>
+              </template>
+            </ul>
+          </li>
+        </template>
+        <template v-else-if="groupName !== 'geral' && shouldRenderGroup(items)">
+          <li>
+            <button type="button" v-if="items.title"
+              class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              @click="toggleDropdown(groupName)">
+              <i :class="items.icon"></i>
+              <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ items.title }}</span>
+              <svg :class="{ 'transform rotate-180': isDropdownOpen(groupName) }" class="w-3 h-3" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+            <ul v-show="isDropdownOpen(groupName)" class="py-2 space-y-2">
+              <template v-for="(item, index) in items?.items" :key="index">
+                <li v-if="hasPermission(item)">
+                  <router-link :to="{ name: item.to }"
+                    class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
+                    <span class="ml-3">{{ item.title }}</span>
+                  </router-link>
+                </li>
+              </template>
+            </ul>
+          </li>
+        </template>
+        <template v-else>
+          <template v-for="(item, index) in items.items" :key="index">
+            <li v-if="hasPermission(item)">
+              <router-link :to="{ name: item.to }" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white"
+                :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
+                <i :class="item.icon"></i>
+                <span class="ml-3">{{ item.title }}</span>
+              </router-link>
             </li>
-          </template>
-          <template v-else-if="groupName !== 'geral' && shouldRenderGroup(items)">
-            <li>
-              <button type="button" v-if="items.title"
-                class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                @click="toggleDropdown(groupName)">
-                <i :class="items.icon"></i>
-                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ items.title }}</span>
-                <svg :class="{ 'transform rotate-180': isDropdownOpen(groupName) }" class="w-3 h-3" aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 4 4 4-4" />
-                </svg>
-              </button>
-              <ul v-show="isDropdownOpen(groupName)" class="py-2 space-y-2">
-                <template v-for="(item, index) in items?.items" :key="index">
-                  <li v-if="hasPermission(item)">
-                    <router-link :to="{ name: item.to }"
-                      class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                      :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
-                      <span class="ml-3">{{ item.title }}</span>
-                    </router-link>
-                  </li>
-                </template>
-              </ul>
-            </li>
-          </template>
-          <template v-else>
-            <template v-for="(item, index) in items.items" :key="index">
-              <li v-if="hasPermission(item)">
-                <router-link :to="{ name: item.to }" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white"
-                  :class="{ 'bg-gray-100 dark:bg-gray-700': isRouteActive(item.to) }">
-                  <i :class="item.icon"></i>
-                  <span class="ml-3">{{ item.title }}</span>
-                </router-link>
-              </li>
-            </template>
           </template>
         </template>
-      </ul>
-    </div>
-  </template>
+      </template>
+    </ul>
+  </div>
+</template>
   
   <script>
   import { onMounted, ref, computed } from "vue";
@@ -84,13 +84,6 @@
       const loading       = ref(false);
       const openDropdowns = ref([]);
       const user          = props?.user;
-  
-    //   const logout = () => {
-    //     loading.value = true;
-    //     store.dispatch("logout")
-    //       .then(() => router.push({ name: "login" }))
-    //       .finally(() => (loading.value = false));
-    //   };
   
       const isRouteActive = (routeName) => {
         return router.currentRoute.value.name === routeName;
@@ -154,7 +147,6 @@
   
       return {
         user,
-        // logout,
         groupedSidebar,
         isRouteActive,
         toggleDropdown,
