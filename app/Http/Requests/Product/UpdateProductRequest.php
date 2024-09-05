ca0a8928d0ca5600f5ccfe8d6658e1c4c1c1dc6a
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-05 15:53:29 -0400

    fix: :fire: remove image attribute in UpdateProductRequest from laravel api

--- app/Http/Requests/Product/UpdateProductRequest.php
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -23,12 +23,12 @@ class UpdateProductRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    {
+    {    
+
         return [
             'name'              => ['required', 'string', 'max:50'],
             'price'             => ['required', 'numeric'],
             'description'       => ['nullable', 'string', 'max:200'],
-            'image'             => ['nullable', 'file', 'mimes:jpeg,jpg,png'],
             'expiration_date'   => ['required', 'date', 'after:today'],
             'category_id'       => ['required', 'numeric'],
         ];
