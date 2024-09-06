5aea35ea3789acdd277fa0fbedffc1f057d508a9
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-06 15:39:56 -0400

    feat(laravel api backend): :sparkles: add register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\UserRegistered;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\SignUpFormRequest;
 use App\Http\Resources\UserResource;
@@ -11,8 +12,11 @@ class RegisterController extends Controller
 {
     public function __invoke(SignUpFormRequest $request)
     {
-        $input  =   $request->validated();
-        $user   =   User::create($input);
+        $input          =   $request->validated();
+        $input['token'] = \Str::uuid();
+        $user           =   User::create($input);
+        event(new UserRegistered($user));
+
         return response()->json(UserResource::make($user), 201);
     }
 }
