45c643bac7c8ae6e1b276937816d1c318fb9ecae
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-05 21:32:26 -0400

    fix(vue application frontend): :art: correct columns in the roles table

--- app-vue/src/ui/pages/ACL/Roles/Index.vue
+++ app-vue/src/ui/pages/ACL/Roles/Index.vue
@@ -12,13 +12,15 @@
                                 <Spinner v-if="roleStore.loading" :loading="roleStore.loading" />
                             </th>
                             <th class="px-4 py-2 text-white bg-gray-800 border-b">Nome</th>
-                            <th class="px-4 py-2 text-white bg-gray-800 border-b"></th>
+                            <th class="px-4 py-2 text-white bg-gray-800 border-b">Label</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white dark:bg-gray-800 dark:border-gray-700" v-for="role in roles.data"
-                            :key="role?.id">
+                        <tr class="bg-white dark:bg-gray-800 dark:border-gray-700" v-for="role in roles.data" :key="role?.id">
                             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"></td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ role?.name }}
+                            </td>
                             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ role?.label }}
                             </td>
