df5e6dbec83f7ceb9db23ee768ae8c988577ff99
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-05 21:40:02 -0400

    feat(vue application frontend): :sparkles: add setRole in users store at app-vue application

--- app-vue/src/store/users.ts
+++ app-vue/src/store/users.ts
@@ -1,27 +1,49 @@
 import { defineStore } from "pinia";
 import UsersService from "../infra/services/users.service"
 
-export const useUserStore = defineStore('users', {
+export const useUsersStore = defineStore('users', {
     state: () => ({
         users: [],
+        user: {},
         isLoading: false
     }),
+    getters: {
+        loading (state: any) {
+            return state.isLoading
+        }
+    },
     actions: {
         async getUsers() {
+            this.isLoading = true
             return await UsersService.users().then((response: any) => {
                 this.users = response.data
-            })
+            }).finally(() => this.isLoading = false)
+        },
+        async getUser(id: number) {
+            await UsersService
+                .getUser(id)
+                .then((response: any) => {
+                    return this.user = response.data.data;
+                })
         },
         async saveUser(params: any) {
             this.isLoading = true
             await UsersService
                 .storeUser(params)
-                .then((response: any) => {
-                    return this.users = response.data.data;
-                }).finally(() => {
+                .then((response: any) => this.users = response.data.data)
+                .finally(() => {
                     this.getUsers()
                     this.isLoading = false
                 })
         },
+        async setRole(params: any) {
+            await UsersService
+                .setRole(params)
+                .then((response: any) => this.users = response.data.data)
+                .finally(() => {
+                    this.getUsers()
+                    this.isLoading = false
+                })
+        }
     }
 });
