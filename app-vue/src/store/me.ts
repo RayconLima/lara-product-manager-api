e7efc26d89dfbbf1db9133164e0759bb233a776e
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-09 15:46:39 -0400

    feat(vue application frontend): :sparkles: add type interface to me store pinia in app-vue application

--- app-vue/src/store/me.ts
+++ app-vue/src/store/me.ts
@@ -1,16 +1,42 @@
 import { defineStore } from 'pinia';
 import AuthService from '../infra/services/auth.service'
 
+interface PermissionUser {
+  id: number;
+  name: string;
+  label: string;
+  created_at: null | string;
+  updated_at: null | string;
+}
+
+interface UserRole {
+  id: number;
+  name: string;
+  label: string;
+  created_at: null | string;
+  updated_at: null | string;
+  permissions: PermissionUser[];
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  avatar: string;
+  roles: UserRole[];
+  permissions: string[];
+}
+
 export const useMeStore = defineStore('me', {
   state: () => ({
-    user: null,
+    user: {} as User,
   }),
 
   actions: {
     async getMe() {
       try {
         return await AuthService.getMe().then(async (response:any) => {
-          this.user   =   await response.data
+          this.user = await response.data
         });
       } catch (error) {
         throw error;
