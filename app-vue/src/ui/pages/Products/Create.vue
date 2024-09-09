f292d94bd7a8d1a752bbfcaf672477c9d44f86e3
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-09 16:50:47 -0400

    refactor(vue application frontend): :fire: remove config line in Create Product in app-vue application

--- app-vue/src/ui/pages/Products/Create.vue
+++ app-vue/src/ui/pages/Products/Create.vue
@@ -104,7 +104,6 @@ export default {
         return {
             categories,
             form,
-            config,
             imagePreview,
             newProduct,
             handleImageChange,
