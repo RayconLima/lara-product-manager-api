aa12f4a7943f41982c2e32d8281f4a1bc43575df
Raycon Lima Batista <rayconlimabatista18@gmail.com>
2024-09-09 16:48:39 -0400

    feat(vue application frontend): :sparkles: add info card at dashboard page in app-vue application

---
+++ app-vue/src/ui/components/CardDash.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="`p-4 text-white`" :style="`background-color: ${color}`">
+        <h2 class="text-xl font-semibold">{{ title }}</h2>
+        <p class="text-3xl">{{ dataCount }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "CardDash",
+    props: {
+        title: {
+            type: String,
+            default: "Card",
+        },
+        color: {
+            type: String,
+            default: "blue",
+        },
+        dataCount: {
+            type: Number,
+            default: 0,
+        },
+    }
+}
+</script>
